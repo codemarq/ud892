c3fcd810e1660703d7e60791c887fe48e89f9b87
John-Mike Marquardt <jmmarquardt@gmail.com>
2017-04-22 23:27:40 -0400

    image compression

--- me/gulpfile.js
+++ me/gulpfile.js
@@ -9,6 +9,9 @@ const jasmine 			= require('gulp-jasmine-phantom');
 const concat				=	require('gulp-concat');
 const uglify				=	require('gulp-uglify');
 const babel					= require('gulp-babel');
+const sourcemaps		=	require('gulp-sourcemaps');
+const imagemin			=	require('gulp-imagemin');
+const pngquant			= require('imagemin-pngquant');
 
 gulp.task('default', ['styles', 'lint', 'copy-html', 'copy-image', 'scripts'], () => {
 	// console.log('gulp is running');
@@ -40,7 +43,7 @@ gulp.task('lint', () => {
 	// So, it's best to have gulp ignore the directory as well.
 	// Also, Be sure to return the stream from the task;
 	// Otherwise, the task may end before the stream has finished.
-	return gulp.src(['**/*.js','!node_modules/**'])
+	return gulp.src(['**/*.js','!node_modules/**', '!tests/**'])
 		// eslint() attaches the lint output to the "eslint" property
 		// of the file object so it can be used by other modules.
 		.pipe(eslint())
@@ -67,20 +70,28 @@ gulp.task('copy-html', () => {
 
 gulp.task('copy-image', () => {
 	gulp.src('./img/*')
+		.pipe(imagemin({
+			progressive: true,
+			use: [pngquant()]
+		}))
 		.pipe(gulp.dest('./build/img'));
 });
 
 gulp.task('scripts', () => {
 	gulp.src('./js/**/*.js')
+		.pipe(sourcemaps.init())
 		.pipe(babel())
 		.pipe(concat('all.js'))
+		.pipe(sourcemaps.write())
 		.pipe(gulp.dest('./build/js'));
 });
 
 gulp.task('scripts-build', () => {
 	gulp.src('./js/**/*.js')
+		.pipe(sourcemaps.init())
 		.pipe(babel())
 		.pipe(concat('all.js'))
 		.pipe(uglify())
+		.pipe(sourcemaps.write())
 		.pipe(gulp.dest('./build/js'));
 });
